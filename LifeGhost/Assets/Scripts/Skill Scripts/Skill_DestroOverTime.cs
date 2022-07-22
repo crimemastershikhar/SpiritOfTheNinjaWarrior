2c42600617e459e42f34c4b472c796eb08fd8ae7
Shikhar Garg <shikherbest5@gmail.com>
2022-07-22 18:50:40 +0530

    Added Skills placements
    Imported packages containing skills and added Skills GO

--- LifeGhost/Assets/Scripts/Skill Scripts/Skill_DestroOverTime.cs
+++ LifeGhost/Assets/Scripts/Skill Scripts/Skill_DestroOverTime.cs
@@ -1,18 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Skill_DestroOverTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float timer;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Destroy(gameObject, timer);
     }
 }
