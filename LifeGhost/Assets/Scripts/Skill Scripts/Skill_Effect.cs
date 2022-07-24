a0e354134638a4b46d37cdc7e3352eaf5a1f034a
Shikhar Garg <shikherbest5@gmail.com>
2022-07-24 12:28:30 +0530

    Preparing skills scripts CommitV2

---
+++ LifeGhost/Assets/Scripts/Skill Scripts/Skill_Effect.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class summary: To detect with which game object this fireball is colliding i.e. Zombies or the boss
+public class Skill_Effect : MonoBehaviour
+{
+    
+}
--- LifeGhost/Assets/Scripts/Skill Scripts/Skill_PrefabGenerator.cs
+++ LifeGhost/Assets/Scripts/Skill Scripts/Skill_PrefabGenerator.cs
@@ -1,19 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Class Summary:  Generate Prefab out of the arrows
+//Class Summary:  Generate Prefab out of the arrows.||  Also using timercounter to control the spawn rate 
 public class Skill_PrefabGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] skillPrefabs;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private int thisManyTime = 3;  //how many times to create variables
+    [SerializeField] private float overThisTime = 3f; // over whch interval should we create
+    
+    [SerializeField] private float x_Width, y_Width, z_Width;   //position our GO 
+    [SerializeField] private float x_RotMax, y_RotMax = 180f, z_RotMax;
+
+    private bool allUseSameRotation = false;  //determine evry prefab that we create use same rota. as before
+    private bool allRotationDecided = false;  // determine that every rot. for every GO is decided
+
+    private float x_Cur, y_Cur, z_Cur;
+    private float x_RotCur, y_RotCur, z_RotCur; 
+    
+    private int randNum;  //spawn random prefabs
+    private float trigger;
+    
+    private float timeCounter;  //How many times we call this function
+    private float effectCounter;
+
+    private void Start()
     {
-        
+        trigger = overThisTime / thisManyTime;  //Define interval of time
     }
 }
