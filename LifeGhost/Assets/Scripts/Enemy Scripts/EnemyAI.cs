5a6a7296cca88fcbe15fb49a9a068a901fdd4ad6
Shikhar Garg <shikherbest5@gmail.com>
2022-07-26 23:28:14 +0530

    Navmesh AI working
    Impleneted Chase and attack state machines between 1 zombie and player

--- LifeGhost/Assets/Scripts/Enemy Scripts/EnemyAI.cs
+++ LifeGhost/Assets/Scripts/Enemy Scripts/EnemyAI.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -22,7 +24,9 @@ public class EnemyAI : MonoBehaviour
     private CapsuleCollider col;
     private Transform zombieTransform;
 
-
+    private NavMeshAgent navAgent;
+    [SerializeField] private Transform[] navPoints;
+    private int naviagationIndex;
 
     private void Awake()
     {
@@ -32,23 +36,43 @@ public class EnemyAI : MonoBehaviour
         zombieTransform = this.transform;
         enemyHealth = GetComponent<EnemyHealth>();
         playerHealth = player.gameObject.GetComponent<PlayerHealth>();
+        
+        navAgent = GetComponent<NavMeshAgent>();
+        naviagationIndex = Random.Range(0, navPoints.Length);  //determine where zombie is going to go first 
+        navAgent.SetDestination(navPoints[naviagationIndex].position);  //pos where GO needs to go 
 
     }
 
     private void Update()
     {
         float distance = Vector3.Distance(zombieTransform.position, player.position);
+
         if (enemyHealth.realHealth > 0)
         {
-            if (distance > 2.5f)
-                Chase();
-        }
-        else
-        {
-            Attack();
+            if (distance > 7f)
+            {
+                Search();
+                navAgent.isStopped = false;
+            }
+            else
+            {
+                navAgent.isStopped = true;
+
+                if (distance > 2.5f)
+                {
+                    Chase();
+                }
+                else
+                {
+                    Attack();
+                }
+                
+                transform.LookAt(player);
+
+            }
+            
         }
         
-        transform.LookAt(player);
     }
 
     void Chase()
@@ -64,8 +88,34 @@ public class EnemyAI : MonoBehaviour
         anim.SetBool(ANIMATION_ATTACK, true);
         
     }
-    
-    
+
+    void Search()
+    {
+        if (navAgent.remainingDistance <= 0.5f)
+        {
+            anim.SetFloat(ANIMATION_SPEED,0f);
+            anim.SetBool(ANIMATION_ATTACK,false);
+            anim.SetBool(ANIMATION_RUN,false);
+
+            if (naviagationIndex == navPoints.Length - 1)
+            {
+                naviagationIndex = 0;
+            }
+            else
+            {
+                naviagationIndex++;
+            }
+
+            navAgent.SetDestination(navPoints[naviagationIndex].position);
+        }
+        else
+        {
+            anim.SetFloat(ANIMATION_SPEED,1f);
+            anim.SetBool(ANIMATION_ATTACK,false);
+            anim.SetBool(ANIMATION_RUN,false);
+        }
+        
+    }
     
     
     
